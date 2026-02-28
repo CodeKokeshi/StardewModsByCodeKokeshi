7fcd361735ab7ad9900811efb3b264ecf55abca2
CodeKokeshi <kokeshiaikawa003@gmail.com>
2026-02-28 10:54:37 +0800

    Add project files and configuration for MaxQualityManual
    - Created sourcelink.json for source linking.
    - Added nuget.dgspec.json for NuGet restore specifications.
    - Included nuget.g.props and nuget.g.targets for package management.
    - Generated project.assets.json to manage project dependencies.
    - Established project.nuget.cache for caching NuGet project state.

--- Officially Finished/MaxQualityAuto/IGenericModConfigMenuApi.cs
+++ Officially Finished/MaxQualityAuto/IGenericModConfigMenuApi.cs
@@ -1,8 +1,7 @@
 using System;
 using StardewModdingAPI;
-using StardewModdingAPI.Utilities;
 
-namespace MaxQualityManual;
+namespace MaxQualityAuto;
 
 /// <summary>The API which lets other mods add a config UI through Generic Mod Config Menu.</summary>
 public interface IGenericModConfigMenuApi
@@ -10,5 +9,5 @@ public interface IGenericModConfigMenuApi
     void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
     void AddSectionTitle(IManifest mod, Func<string> text, Func<string>? tooltip = null);
     void AddParagraph(IManifest mod, Func<string> text);
-    void AddKeybindList(IManifest mod, Func<KeybindList> getValue, Action<KeybindList> setValue, Func<string> name, Func<string>? tooltip = null, string? fieldId = null);
+    void AddBoolOption(IManifest mod, Func<bool> getValue, Action<bool> setValue, Func<string> name, Func<string>? tooltip = null, string? fieldId = null);
 }
